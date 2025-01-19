2fbebae301f1ce9244ba21f6f2d8ec08968628f0
Nils Müller <tyriis@users.noreply.github.com>
2025-01-20 00:56:46 +0100

    chore: cleanup validation formating :(

--- src/config_validator.go
+++ src/config_validator.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"os"
-	"strings"
 
 	"github.com/rs/zerolog/log"
 	"github.com/santhosh-tekuri/jsonschema/v6"
@@ -13,14 +12,15 @@ func validateConfig() {
 	// Read and parse the YAML file
 	yamlData, err := os.ReadFile("config/config.yaml")
 	if err != nil {
-		log.Error().Err(err).Msg("Error reading YAML file")
+		log.Fatal().Err(err).Msg("Error reading YAML config file")
 		return
 	}
 
+	// Unmarshal the YAML data
 	var data interface{}
 	err = yaml.Unmarshal(yamlData, &data)
 	if err != nil {
-		log.Error().Err(err).Msg("Error parsing YAML")
+		log.Fatal().Err(err).Msg("Error parsing YAML config")
 		return
 	}
 
@@ -28,7 +28,7 @@ func validateConfig() {
 	compiler := jsonschema.NewCompiler()
 	schema, err := compiler.Compile("config/schema.json")
 	if err != nil {
-		log.Error().Err(err).Msg("Error compiling schema")
+		log.Fatal().Err(err).Msg("Error compiling jsonschema")
 		return
 	}
 
@@ -37,9 +37,6 @@ func validateConfig() {
 	if err != nil {
 		if validationError, ok := err.(*jsonschema.ValidationError); ok {
 			log.Fatal().Msg(validationError.Error())
-			// for _, outputUnit := range validationError.DetailedOutput().Errors {
-			// 	handleErrors(outputUnit, validationError)
-			// }
 		} else {
 			log.Fatal().Err(err).Msg("Validation error")
 		}
@@ -47,51 +44,4 @@ func validateConfig() {
 	}
 
 	log.Info().Msg("Configuration is valid")
-}
-
-func handleErrors(outputUnit jsonschema.OutputUnit, validationErr *jsonschema.ValidationError) {
-	if outputUnit.Errors != nil {
-		for _, item := range outputUnit.Errors {
-			if item.Errors != nil {
-				handleErrors(item, validationErr)
-			} else {
-				handleError(item, item.Error, validationErr)
-			}
-		}
-	} else {
-		handleError(outputUnit, outputUnit.Error, validationErr)
-	}
-}
-
-func handleError(detail jsonschema.OutputUnit, outputErr *jsonschema.OutputError, validationErr *jsonschema.ValidationError) {
-	// if additionalProps, ok := outputErr.Kind.(*kind.AdditionalProperties); ok {
-	// 	for _, property := range additionalProps.Properties {
-	// 		path := formatYAMLKey(detail.InstanceLocation)
-	// 		var propertyPath string
-	// 		if path == "" {
-	// 			propertyPath = property
-	// 		} else {
-	// 			propertyPath = fmt.Sprintf("%s.%s", path, property)
-	// 		}
-	// 		log.Fatal().
-	// 			Msgf("invalid key: '%s'", propertyPath)
-	// 	}
-	// } else if typeErr, ok := outputErr.Kind.(*kind.Type); ok {
-	// 	path := formatYAMLKey(detail.InstanceLocation)
-	// 	log.Fatal().
-	// 		Str("got", typeErr.Got).
-	// 		Strs("want", typeErr.Want).
-	// 		Msgf("invalid type for '%s'", path)
-	// } else if requiredErr, ok := outputErr.Kind.(*kind.Required); ok {
-	// 	path := formatYAMLKey(detail.InstanceLocation)
-	// 	log.Fatal().
-	// 		Msgf("missing key: '%s.%s'", path, requiredErr.Missing[0])
-	// } else {
-	log.Error().Msg("Config validation error")
-	log.Fatal().Msg(validationErr.Error())
-	// }
-}
-
-func formatYAMLKey(input string) string {
-	return strings.ReplaceAll(strings.TrimPrefix(input, "/"), "/", ".")
 }
